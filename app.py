f5b768d7e248d9ce9392aa6e4b0a056ea33c3a08
yohanderose <yohanderose@gmail.com>
2018-09-29 09:53:13 +0000

    cam problems

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, Response
+import picamera
+import io
+import time
+    
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
+    """Video streaming ."""
     return render_template('index.html')
 
+def gen(camera):
+    while True:
+        camera.capture('p.jpg')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' +
+               open('p.jpg').read().decode('utf-8') +
+               b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='localhost')
+    camera = picamera.PiCamera()
+    camera.start_preview()
+    time.sleep(1)
+    
+    app.run(debug=True, port=81, host='0.0.0.0',
+            ssl_context=('cert.pem', 'key.pem'))
+    
